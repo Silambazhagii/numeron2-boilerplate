fc2d628344437b5b4e4fccc2b2a33d5d978def04
Silambazhagii <silambazhagii@gmail.com>
2023-10-26 18:15:55 +0530

    updated

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // Iteration 1: Making the play button in the index.html functional.
 // Description: When the play button is clicked the game.html page should be opened
+
+const play_btn = document.getElementById("play-button")
+
+play_btn.addEventListener("click",function(){
+    window.location.href="game.html"
+})
--- score.js
+++ score.js
@@ -1,1 +1,11 @@
 // Iteration 8: Making scoreboard functional
+
+const score = document.getElementById('score-board');
+const playbtn = document.getElementById('play-again-button')
+
+playbtn.addEventListener('click',()=>{
+    window.location.href='game.html'
+    sessionStorage.removeItem('scr')
+})
+score.innerText = sessionStorage.getItem("scr") || 0;
+
